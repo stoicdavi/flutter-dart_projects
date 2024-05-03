777a4d4eafb4684e994262ef36c6e063eba1758a
David Nanjila <nanjiladavid2@gmail.com>
2024-05-03 19:50:00 +0300

    Chore: wrap the column with center to make the contents of the app centered

--- namer_app/lib/main.dart
+++ namer_app/lib/main.dart
@@ -42,18 +42,20 @@ class MyHomePage extends StatelessWidget {
     var pair = appState.current;
 
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('A random AWESOMe idea:'),
-          BigCard(pair: pair),
-          ElevatedButton(
-            onPressed: (){
-              appState.getNext();
-            },
-            child: Text('Next'),
-          )
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('A random AWESOMe idea:'),
+            BigCard(pair: pair),
+            ElevatedButton(
+              onPressed: (){
+                appState.getNext();
+              },
+              child: Text('Next'),
+            )
+          ],
+        ),
       ),
     );
   }
