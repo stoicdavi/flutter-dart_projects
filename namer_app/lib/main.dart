dd711f08d1efeb507582b2b6a2ad77c9a40d023b
David Nanjila <nanjiladavid2@gmail.com>
2024-05-03 20:10:37 +0300

    Feat: add a button to allow user to like his/her favorite random word

--- namer_app/lib/main.dart
+++ namer_app/lib/main.dart
@@ -52,6 +52,13 @@ class MyHomePage extends StatelessWidget {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
 
+    IconData icon;
+    if(appState.favorites.contains(pair)){
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
     return Scaffold(
       body: Center(
         child: Column(
@@ -60,11 +67,24 @@ class MyHomePage extends StatelessWidget {
             Text('random words:'),
             BigCard(pair: pair),
             SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: (){
-                appState.getNext();
-              },
-              child: Text('Next'),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ElevatedButton.icon(
+                  onPressed:(){
+                    appState.toggleFavorite();
+                  },
+                  icon: Icon(icon),
+                  label: Text('Favorite'),
+                ),
+
+                ElevatedButton(
+                  onPressed: (){
+                    appState.getNext();
+                  },
+                  child: Text('Next'),
+                ),
+              ],
             )
           ],
         ),
