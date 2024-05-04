e8e2150c3651ef76dc8d108c25d45e554a81100e
David Nanjila <nanjiladavid2@gmail.com>
2024-05-04 13:29:05 +0300

    Feat: set extended to true to show the lable next to icons, home icon and favorite icon

--- namer_app/lib/main.dart
+++ namer_app/lib/main.dart
@@ -56,7 +56,7 @@ class MyHomePage extends StatelessWidget {
         children: [
           SafeArea(
             child: NavigationRail(
-              extended: false,
+              extended: true,
               destinations: [
                 NavigationRailDestination(
                   icon: Icon(Icons.home),
