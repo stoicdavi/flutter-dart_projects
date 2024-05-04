20f8903942438cba1d7b051b6fba57916d7a627b
David Nanjila <nanjiladavid2@gmail.com>
2024-05-04 13:54:24 +0300

    Feat: allow navigation to the favorites page when the user click the favorites icon

--- namer_app/lib/main.dart
+++ namer_app/lib/main.dart
@@ -57,6 +57,17 @@ class _MyHomePageState extends State<MyHomePage> {
   var selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex){
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        page = Placeholder();
+        break;
+      default:
+        throw UnimplementedError('No widget for: $selectedIndex');
+    }
     return Scaffold(
       body: Row(
         children: [
@@ -84,7 +95,8 @@ class _MyHomePageState extends State<MyHomePage> {
           Expanded(
             child: Container(
               color: Theme.of(context).colorScheme.primaryContainer,
-              child: GeneratorPage(),
+              
+              child: page,
             ),
           ),
         ],
