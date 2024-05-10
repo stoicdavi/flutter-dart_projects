9a3c01e595e642af3e733092e26afb5704e3fee1
David Nanjila <nanjiladavid2@gmail.com>
2024-05-10 23:47:48 +0300

    initial commit

---
+++ testing_app/lib/models/favorites.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Favorrts extends ChangeNotifier {
+  final List<int> _favoriteItems = [];
+
+  List<int> get items => _favoriteItems;
+
+  void add(int itemNo){
+    _favoriteItems.add(itemNo);
+    notifyListeners();
+  }
+  void remove(int itemNo){
+    _favoriteItems.remove(itemNo);
+    notifyListeners();
+  }
+}
