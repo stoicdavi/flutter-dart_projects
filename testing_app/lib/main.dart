e2f91b052a1a7f39613cb12d05b808e07023f0d6
David Nanjila <nanjiladavid2@gmail.com>
2024-05-11 11:59:38 +0300

    Feat: Create a new test file

--- testing_app/lib/main.dart
+++ testing_app/lib/main.dart
@@ -1,20 +1,45 @@
-import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+import 'models/favorites.dart';
+import 'screens/home.dart';
+import 'screens/favorites.dart';
+void main(){
+  runApp(const TestingApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+final _router = GoRouter(
+  routes: [
+    GoRoute(path: HomePage.routeName,
+      builder:(context, state){
+        return const HomePage();
+        },
+        routes: [
+          GoRoute(path: FavoritesPage.routeName,
+            builder: (context, state){
+            return const FavoritesPage();
+            },
+          ),
+        ]
+    ),
+  ],
+);
 
+class TestingApp extends StatelessWidget {
+  const TestingApp({super.key});
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+  Widget build(BuildContext context){
+    return ChangeNotifierProvider<Favorites>(
+      create: (context) => Favorites(),
+      child: MaterialApp.router(
+        title:'Testing Sample',
+        theme:ThemeData(
+          primarySwatch: Colors.blue,
+          useMaterial3:true,
+          ),
+          routerConfig:_router,
         ),
-      ),
     );
   }
 }
--- testing_app/lib/models/favorites.dart
+++ testing_app/lib/models/favorites.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
-
-class Favorrts extends ChangeNotifier {
+//The [ChangeNotifier] class is a class in the Flutter SDK that provides a change notification mechanism.
+// It is used to notify the listeners when the state of an object changes.
+// The [Favorites] class extends the [ChangeNotifier] class to notify the listeners when the state of the favorite items changes.
+// The [Favorites] class has a private list of integers, _favoriteItems, that stores the favorite item numbers.
+// The [items] getter method returns the list of favorite item numbers.
+// The [Favorites] class has two methods, add and remove, to add and remove an item number from the list of favorite items, respectively.
+class Favorites extends ChangeNotifier {
   final List<int> _favoriteItems = [];
 
   List<int> get items => _favoriteItems;
